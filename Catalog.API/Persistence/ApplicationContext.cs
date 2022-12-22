294dacfccbe404d5caa67e41f0d29778495a7075
cypher <m1k4z@yandex.ru>
2022-12-22 09:42:51 +0300

    Initial commit

---
+++ Catalog.API/Persistence/ApplicationContext.cs
@@ -0,0 +1,39 @@
+﻿namespace Catalog.API.Persistence
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            if (Products.ToList().Count == 0)
+            {
+                InitializeProductDefault();
+            }
+        }
+
+        private void InitializeProductDefault()
+        {
+            Products.AddRange(
+                new Product()
+                {
+                    Name = "apple1",
+                    Price = 125252.32M
+                },
+                new Product()
+                {
+                    Name = "samsung1",
+                    Price = 42443.32M
+                },
+                new Product()
+                {
+                    Name = "mio",
+                    Price = 21222.23M
+                }
+            );
+
+            this.SaveChanges();
+        }
+    }
+}
---
+++ Catalog.API/Persistence/ICatalogRepository.cs
@@ -0,0 +1,16 @@
+﻿using CatalogService.API.Entities;
+using System.Collections.Generic;
+
+namespace CatalogService.API.Persistence
+{
+    public interface ICatalogRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product GetItemById(int id);
+        void Create(Product item);
+
+        void Update(Product item);
+        void Delete(int id);
+        void Delete(Product item);
+    }
+}
