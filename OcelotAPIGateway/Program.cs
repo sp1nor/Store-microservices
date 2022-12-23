0374f8167bccd0cf7697d888a6569407558b613b
cypher <m1k4z@yandex.ru>
2022-12-23 08:41:38 +0300

    working ocelot

--- OcelotAPIGateway/Program.cs
+++ OcelotAPIGateway/Program.cs
@@ -1,6 +1,17 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile("ocelot.json")
+                            .Build();
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOcelot(configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseOcelot();
+
+app.MapGet("/api", () => "Hello World!");
 
 app.Run();
